1916f58a424b92e6f42dde34ffa9bc6c26bd5faa
saasbook <saasbook@saasbook.(none)>
2013-08-02 04:41:12 -0700

    hw3 1 created db

--- features/step_definitions/movie_steps.rb
+++ features/step_definitions/movie_steps.rb
@@ -2,10 +2,12 @@
 
 Given /the following movies exist/ do |movies_table|
   movies_table.hashes.each do |movie|
+	Movie.create(:title => movie[:title], :rating => movie[:rating], :release_date => movie[:release_date])
+    #debugger
     # each returned element will be a hash whose key is the table header.
     # you should arrange to add that movie to the database here.
   end
-  flunk "Unimplemented"
+  #flunk "Unimplemented"
 end
 
 # Make sure that one string (regexp) occurs before or after another one
