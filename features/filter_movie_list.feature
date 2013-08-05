e156084b138e6645cc45fa71a738c84b75c30c4e
saasbook <saasbook@saasbook.(none)>
2013-08-05 01:03:48 -0700

    hw3 happy paths for filtering movies - working

--- features/filter_movie_list.feature
+++ features/filter_movie_list.feature
@@ -20,14 +20,33 @@ Background: movies have been added to database
   | Chicken Run             | G      | 21-Jun-2000  |
 
   And  I am on the RottenPotatoes home page
-  And  I check the following ratings: G, PG, R
+  
   
 Scenario: restrict to movies with 'PG' or 'R' ratings
   # enter step(s) to check the 'PG' and 'R' checkboxes
+	When I check the following ratings: PG, R
+	
   # enter step(s) to uncheck all other checkboxes
+	When I uncheck the following ratings: PG-13, NC-17, G
+
   # enter step to "submit" the search form on the homepage
+	When I press "ratings_submit"
+	
+
   # enter step(s) to ensure that PG and R movies are visible
+	Then I should be on the home page
+	And I should see "Amelie"
+	And I should see "Raiders of the Lost Ark"
+	And I should see "The Incredibles"
+	And I should see "The Terminator"
+	And I should see "When Harry Met Sally"
+	  
   # enter step(s) to ensure that other movies are not visible
+	And I should not see "The Help"
+	And I should not see "Chocolat"
+	And I should not see "Aladdin"
+	And I should not see "2001: A Space Odyssey"
+	And I should not see "Chicken Run"
 
 Scenario: no ratings selected
   # see assignment
--- features/step_definitions/movie_steps.rb
+++ features/step_definitions/movie_steps.rb
@@ -11,16 +11,35 @@ Given /the following movies exist/ do |movies_table|
 end
 
 # My Shit Starts here
-When /^(?:|I )check "([^"]*)"$/ do |ratings|
-  ratings.split(',').each do |rating|
-  	debugger
-  	check(field)
+
+When /I (un)?check the following ratings: (.*)/ do |uncheck, rating_list|
+  if uncheck == "un"
+    rating_list.split(', ').each {|x| step %{I uncheck "ratings_#{x}"}}
+  else
+    rating_list.split(', ').each {|x| step %{I check "ratings_#{x}"}}
   end
+  # HINT: use String#split to split up the rating_list, then
+  # iterate over the ratings and reuse the "When I check..." or
+  # "When I uncheck..." steps in lines 89-95 of web_steps.rb
 end
 
-When /^(?:|I )uncheck "([^"]*)"$/ do |field|
-  uncheck(field)
+Then /I should not see any of the movies/ do
+  rows = page.all('#movies tr').size - 1
+  assert rows == 0
 end
+
+Then /I should see all of the movies/ do
+  rows = page.all('#movies tr').size - 1
+  assert rows == Movie.count()
+end
+
+Then /I should see "(.*)" before "(.*)"/ do |e1, e2|
+  assert page.body =~ /#{e1}.+#{e2}/m
+  #  ensure that that e1 occurs before e2.
+  #  page.body is the entire content of the page as a string.
+  #flunk "Unimplemented"
+end
+
 
 # My Shit Ends here
 
@@ -28,18 +47,13 @@ end
 # Make sure that one string (regexp) occurs before or after another one
 #   on the same page
 
-Then /I should see "(.*)" before "(.*)"/ do |e1, e2|
-  #  ensure that that e1 occurs before e2.
-  #  page.body is the entire content of the page as a string.
-  flunk "Unimplemented"
-end
 
 # Make it easier to express checking or unchecking several boxes at once
 #  "When I uncheck the following ratings: PG, G, R"
 #  "When I check the following ratings: G"
 
-When /I (un)?check the following ratings: (.*)/ do |uncheck, rating_list|
+#When /I (un)?check the following ratings: (.*)/ do |uncheck, rating_list|
   # HINT: use String#split to split up the rating_list, then
   #   iterate over the ratings and reuse the "When I check..." or
   #   "When I uncheck..." steps in lines 89-95 of web_steps.rb
-end
+#end
